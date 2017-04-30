628b7dcc278deb8672ec6db527ce92ac4ae330db
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-04-30 14:42:57 +0300

    minor changes

--- src/os_test.py
+++ src/os_test.py
@@ -5,11 +5,16 @@ Created on Apr 18, 2017
 '''
 
 import os
-print("os.py:\n")
-print(os.getcwd())
-# To print absolute path on your system
-# os.path.abspath('.') 
- 
-# To print files and directories in the current directory
-# on your system
-# os.listdir('.')
+
+def main():
+    print("os.py:\n")
+    print(os.getcwd())
+    # To print absolute path on your system
+    # os.path.abspath('.') 
+     
+    # To print files and directories in the current directory
+    # on your system
+    # os.listdir('.')
+
+if __name__ == '__main__':
+    main()
