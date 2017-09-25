beda91429756f54ee6eafe59146392cfd14de5f8
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-09-25 11:17:56 +0100

    added .gitignore folder

--- src/logger/log.py
+++ src/logger/log.py
@@ -13,6 +13,7 @@ class Log(object):
     DEBUG = 1
     ERROR = 2
     CRITICAL = 3
+    
         
     def __init__(self, name = "None"):
         '''
@@ -22,16 +23,36 @@ class Log(object):
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name) -2s %(levelname) -2s %(message)s',
                             datefmt='%d,%b %Y %H:%M:%S')
+        self.log_level = {
+            Log.INFO : self.info,
+            Log.DEBUG : self.debug,
+            Log.ERROR : self.error,
+            Log.CRITICAL : self.critical
+        } 
 
+
+    #def log(self, level, param):
+    #    if level == Log.INFO:
+    #        self.info(param)
+    #    if level == Log.DEBUG:
+    #        self.debug(param)
+    #    if level == Log.ERROR:
+    #        self.error(param)
+    #    if level == Log.CRITICAL:
+    #        self.critical(param)
+    
     def log(self, level, param):
-        if level == Log.INFO:
-            self.info(param)
-        if level == Log.DEBUG:
-            self.debug(param)
-        if level == Log.ERROR:
-            self.error(param)
-        if level == Log.CRITICAL:
-            self.critical(param)
+        try:
+            n = int(level)
+            #print (n)
+        except Exception:
+            print ("can't parse level: ", level)
+        else:
+            f = self.log_level.get(level, None)
+            if f != None:
+                f.__call__(param)
+            
+            
     
     def info(self,param):
         self.logger.info(param)
@@ -49,10 +70,17 @@ class Log(object):
 def test():
     log = Log("TEST")
     log.log(Log.INFO, "(Log.INFO), Test log")
-    log.info("Test info log")
-    log.debug("Test debug log")
-    log.error("Test error log")
-    log.critical("Test critical log")
+    log.log(Log.DEBUG, "(Log.DEBUG), Test log")
+    log.log(Log.ERROR, "(Log.ERROR), Test log")
+    log.log(Log.CRITICAL, "(Log.CRITICAL), Test log")
+    #log.info("Test info log")
+    #log.debug("Test debug log")
+    #log.error("Test error log")
+    #log.critical("Test critical log")
+
+    log.log("test", "(Log.INFO), Test log")
+    log.log(7, "(Log.INFO), Test log")
+
         
 if __name__ == '__main__':
     test()
